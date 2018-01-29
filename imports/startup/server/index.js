8af242406047fa89d0024deb05035bd87aa2155e
Roberto Vázquez González <robertovg@precursive.co.uk>
2018-01-29 19:23:33 +0100

    problems with SubscriptionServer form subscriptions-transport-ws and SubscriptionManager from graphql-subscriptions...

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,6 +1,11 @@
 // eslint-disable-next-line
 import { createApolloServer } from 'meteor/apollo';
 import { makeExecutableSchema } from 'graphql-tools';
+import { createServer } from 'http';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
+import { PubSub, SubscriptionManager } from 'graphql-subscriptions';
+// asdf
+import { execute, subscribe } from 'graphql';
 import merge from 'lodash/merge';
 
 // eslint-disable-next-line
@@ -14,11 +19,39 @@ import UserResolvers from '../../api/user/resolvers';
 import UsersSchema from '../../api/users/Users.graphql';
 import UsersResolvers from '../../api/users/resolvers';
 
-// ass
+// Initialize GraphQL subscriptions
+const pubsub = new PubSub();
 
 const schema = makeExecutableSchema({
   typeDefs: [UsersSchema, UserSchema],
   resolvers: merge(UsersResolvers, UserResolvers),
 });
+
+const subscriptionManager = new SubscriptionManager({ schema, pubsub });
 // Just creating a meteor server instance with apollo and injecting the schemas and resolvers
 createApolloServer({ schema });
+
+const WS_PORT = 5000;
+
+// Create WebSocket listener server
+const websocketServer = createServer((request, response) => {
+  response.writeHead(404);
+  response.end();
+});
+
+// Bind it to port and start listening
+websocketServer.listen(WS_PORT, () =>
+  console.log(`Websocket Server is now running on http://localhost:${WS_PORT}`)
+);
+
+const subscriptionServer = SubscriptionServer.create(
+  {
+    schema,
+    execute,
+    subscribe,
+  },
+  {
+    server: websocketServer,
+    path: '/graphql',
+  }
+);
